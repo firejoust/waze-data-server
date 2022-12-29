02af6b5f85274b42703167017b25588145cbab99
firejoust <42525540+firejoust@users.noreply.github.com>
2022-12-29 14:24:37 +1100

    receive http request

--- server.cr
+++ server.cr
@@ -1,8 +1,22 @@
 require "json"
+require "http/client"
 
 config = File.open("./config.json") do |file|
-    Hash(String, Int32 | Float32).from_json(file.gets_to_end)
+    Hash(String, Int32 | Float64).from_json(file.gets_to_end)
 end
 
-puts config
-puts config["interval_mins"]
+def request(config)
+    params = URI::Params.encode({
+        "bottom" => config["pos_bottom"].to_s,
+        "left"   => config["pos_left"].to_s,
+        "ma"     => config["max_alerts"].to_s,
+        "mj"     => config["max_jams"].to_s,
+        "mu"     => config["max_users"].to_s,
+        "right"  => config["pos_right"].to_s,
+        "top"    => config["pos_top"].to_s
+    })
+    response = HTTP::Client.get URI.new("https", "www.waze.com", nil, "/rtserver/web/TGeoRSS/", params)
+    puts response.body
+end
+
+request(config)
